e84c45e0d050927b84ae340492d524c36de527f6
Pablo Rigalli <53019496+pabrig@users.noreply.github.com>
2020-07-10 15:04:38 -0300

    add test

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -3,7 +3,7 @@ import Search from './Search';
 import { shallow } from 'enzyme';
 
 describe('Tests  search', () => {
-	it('test ok', () => {
+	it('test ok ', () => {
 		const wrapper = shallow(<Search />)
 		expect(wrapper).toHaveLength(1);
 	})
